70499b6abe39bf20783c93bb4ae2a1cb1fcf9d4c
Suman <sumanranjan17291729@gmail.com>
2024-02-22 19:13:44 +0530

    second commit

--- 17Pointer/1Basic.cpp
+++ 17Pointer/1Basic.cpp
@@ -3,9 +3,17 @@ using namespace std;
 
 int main(){
     int arr[5] = {1,2,3,4,5};
-    int * ptr = &arr;
 
-    char ch[10] = "Babbar";
-    char * cptr = &ch;
+    //pointer to an arry
+    int * ptr = arr;
+
+    //pointer to an arry
+    int (*ptr1)[5] = &arr;
+
+    cout<<(*ptr1)[0]<<endl;
+
+
+    // char ch[10] = "Babbar";
+    // char * cptr = &ch;
 return 0;
 }
