ca4089ed20f12c10c789ce3408c2813ca6c7c367
Suman <sumanranjan17291729@gmail.com>
2024-02-23 10:50:23 +0530

    second commit

--- 17Pointer/1Basic.cpp
+++ 17Pointer/1Basic.cpp
@@ -1,19 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int arr[5] = {1,2,3,4,5};
+// void solve(int *arr, int size)
+// {
+//     // cout<<sizeof(arr);
+//     // cout << "Inside solve function arr : " << arr << endl;
+//     // cout << "Inside solve function &arr : " << &arr << endl;
+//     *arr = *arr + 1;
+// }
 
-    //pointer to an arry
-    int * ptr = arr;
+void solve(int* &p) {
+	p = p + 1;
+}
 
-    //pointer to an arry
-    int (*ptr1)[5] = &arr;
+int main()
+{
 
-    cout<<(*ptr1)[0]<<endl;
+    int a = 5;
+	int* p = &a;
 
+	cout << "p: " << p << endl;
+	cout << "&p : " << &p << endl;
+	cout << "*p: " << *p << endl;
+	solve(p);
+	cout << "p: " << p << endl;
+	cout << "&p : " << &p << endl;
+	cout << "*p: " << *p << endl;
+
+    // int arr[5] = {10, 20, 30, 40, 50};
+    // // cout<<sizeof(arr)<<endl;
+    // solve(arr, 5);
+
+    // for (int i = 0 ; i<5 ; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // cout << "Inside main function arr : " << arr << endl;
+    // cout << "Inside main function &arr : " << &arr << endl;
+
+    // int arr[5] = {1,2,3,4,5};
+
+    // //pointer to an arry
+    // int * ptr = arr;
+
+    // //pointer to an arry
+    // int (*ptr1)[5] = &arr;
+
+    // cout<<(*ptr1)[0]<<endl;
 
     // char ch[10] = "Babbar";
     // char * cptr = &ch;
-return 0;
+    return 0;
 }
