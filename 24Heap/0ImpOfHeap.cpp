60af085ca0021c30d8ded7a4357a1aef000a3d68
Suman <sumanranjan17291729@gmail.com>
2024-04-16 11:39:48 +0530

    Heap

--- 24Heap/0ImpOfHeap.cpp
+++ 24Heap/0ImpOfHeap.cpp
@@ -43,7 +43,7 @@ public:
                 break;
             }
         }
-    } 
+    }
 
     void printingHeap()
     {
@@ -52,19 +52,127 @@ public:
             cout << arr[i] << " ";
         }
     }
+
+    int deleteFromHeap()
+    {
+        int ans = arr[1];
+        // replacement
+        arr[1] = arr[size];
+        // last element ko delete uski originak position se
+        size--;
+
+        int index = 1;
+        while (index < size)
+        {
+            int leftindex = 2 * index;
+            int rightindex = 2 * index + 1;
+
+            // find out karna h , sabse bada kon
+            int largestkaindex = index;
+            // check left child
+            if (leftindex <= size && arr[largestkaindex] < arr[leftindex])
+            {
+                largestkaindex = leftindex;
+            }
+            if (rightindex <= size && arr[largestkaindex] < arr[rightindex])
+            {
+                largestkaindex = rightindex;
+            }
+            // no change
+            if (index == largestkaindex)
+            {
+                break;
+            }
+
+            else
+            {
+                swap(arr[index], arr[largestkaindex]);
+                index = largestkaindex;
+            }
+        }
+        return ans;
+    }
 };
+
+// most important function ---
+void heapify(int *arr, int n, int index)
+{
+    int leftindex = 2 * index;
+    int rightindex = 2 * index + 1;
+    int largestkaindex = index;
+
+    // teno mai max lao
+    if (leftindex <= n && arr[leftindex] > arr[largestkaindex])
+    {
+        largestkaindex = leftindex;
+    }
+
+    if (rightindex <= n && arr[rightindex] > arr[largestkaindex])
+    {
+        largestkaindex = rightindex;
+    }
+
+    if (index != largestkaindex)
+    {
+        swap(arr[index], arr[largestkaindex]);
+        index = largestkaindex;
+        heapify(arr, n, index);
+    }
+}
+
+void bulidHeap(int arr[], int n)
+{
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+}
+
+void sortHeap(int arr[], int n)  
+{
+    //tc - n log n
+    while (n != 1)
+    {
+        swap(arr[1], arr[n]);
+        n--;
+        heapify(arr, n, 1);
+    }
+}
 
 int main()
 {
-    Heap h(20);
-    h.insert(10);
-    h.insert(20);
-    h.insert(5);
-    h.insert(11);
-    h.insert(6);
 
-    cout << "Printing the content of heap" << endl;
-    h.printingHeap();
+    int arr[] = {-1, 5, 10, 15, 20, 25, 12};
+    int n = 6;
+    bulidHeap(arr, 6);
+
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    sortHeap(arr, n);
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // Heap h(20);
+    // h.insert(10);
+    // h.insert(20);
+    // h.insert(5);
+    // h.insert(11);
+    // h.insert(6);
+
+    // cout << "Printing the content of heap" << endl;
+    // h.printingHeap();
+    // int ans = h.deleteFromHeap();
+    // cout << endl;
+    // cout << "answer : " << ans << endl;
+    // cout << "Printing the content of heap" << endl;
+    // h.printingHeap();
 
     return 0;
 }
