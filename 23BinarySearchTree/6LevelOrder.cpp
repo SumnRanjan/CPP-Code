6be087773919ca946bf02bca718de74d5b65e3b0
Suman <sumanranjan17291729@gmail.com>
2024-04-20 14:43:37 +0530

    BST

---
+++ 23BinarySearchTree/6LevelOrder.cpp
@@ -0,0 +1,41 @@
+// 102. Binary Tree Level Order Traversal
+class Solution {
+public:
+    void solve(TreeNode*& root, vector<vector<int>>& ans) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();    
+            vector<int> levelNodes;
+
+            // Traverse all nodes at current level
+            for (int i = 0; i < size; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                levelNodes.push_back(temp->val); 
+
+                if (temp->left != NULL) {
+                    q.push(temp->left);
+                }
+
+                if (temp->right != NULL) {
+                    q.push(temp->right);
+                }
+            }
+            ans.push_back(levelNodes);
+        }
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == NULL) {
+            return ans;
+        }
+
+        solve(root, ans);
+
+        return ans;
+    }
+};
